ebf86398239f7e2f48569a9a3d3229abc1962dd8
Retro <mossiat.jeoffrey@outlook.com>
2019-09-11 12:10:45 +0200

    Fix: Resolve display.js toggle component Map-Atm

--- src/client/components/display.js
+++ src/client/components/display.js
@@ -1,9 +1,67 @@
+/* eslint-disable react/no-multi-comp */
 import * as React from "react";
 
-function hello() {
-    return "hello";
+class Display extends React.Component {
+    constructor(props) {
+        super(props);
+        this.handleDisplayMap = this.handleDisplayMap.bind(this);
+        this.handleDisplayAtm = this.handleDisplayAtm.bind(this);
+        this.state = {showAtm: false};
+    }
+
+    handleDisplayMap() {
+        this.setState({showAtm: true});
+    }
+
+    handleDisplayAtm() {
+        this.setState({showAtm: false});
+    }
+
+    render() {
+        const showAtm = this.state.showAtm;
+        let button;
+
+        if (showAtm) {
+            button = <MapButton onClick={this.handleDisplayAtm} />;
+        } else {
+            button = <AtmButton onClick={this.handleDisplayMap} />;
+        }
+
+        return (
+            <div>
+                <Greeting showAtm={showAtm} />
+                {button}
+            </div>
+        );
+    }
 }
 
-const Greeting = () => <h1>{hello()}</h1>;
+function Greeting(props) {
+    const showAtm = props.showAtm;
 
-export default Greeting;
+    // display ATM.
+    if (showAtm) {
+        return <h1>{"Modal for the ATM"}</h1>;
+    }
+
+    // display MAP
+    return <h1>{"Modal for the Map."}</h1>;
+}
+
+function AtmButton(props) {
+    return (
+        <button type={"button"} onClick={props.onClick}>
+            {"ATM"}
+        </button>
+    );
+}
+
+function MapButton(props) {
+    return (
+        <button type={"button"} onClick={props.onClick}>
+            {"MAP"}
+        </button>
+    );
+}
+
+export default Display;
