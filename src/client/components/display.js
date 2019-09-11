ba23e52bf67b3fc42052e190ae9cf34880159f9a
Retro <mossiat.jeoffrey@outlook.com>
2019-09-11 14:53:01 +0200

    Fix: Map is able to be toggle with modal && npm install leaflet

--- src/client/components/display.js
+++ src/client/components/display.js
@@ -1,5 +1,41 @@
-/* eslint-disable react/no-multi-comp */
+// Some error to find a fix
+
+/* eslint-disable react/no-multi-comp*/
+
 import * as React from "react";
+
+const x = document.querySelector("#localTarget");
+
+const L = require("leaflet");
+
+function showPosition(position) {
+    x.innerHTML = `Latitude: ${position.coords.latitude}<br>Longitude: ${position.coords.longitude}`;
+
+    const mymap = L.map("mapid").setView(
+        [position.coords.latitude, position.coords.longitude],
+        13,
+    );
+
+    L.tileLayer(
+        "https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw",
+        {
+            maxZoom: 18,
+            attribution:
+                'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
+                '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
+                'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+            id: "mapbox.streets",
+        },
+    ).addTo(mymap);
+}
+
+function getLocation() {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(showPosition);
+    } else {
+        x.innerHTML = "Geolocation is not supported by this browser.";
+    }
+}
 
 class Display extends React.Component {
     constructor(props) {
@@ -16,7 +52,6 @@ class Display extends React.Component {
     handleDisplayAtm() {
         this.setState({showAtm: false});
     }
-
     render() {
         const showAtm = this.state.showAtm;
         let button;
@@ -44,8 +79,14 @@ function Main(props) {
         return <h1>{"Modal for the ATM"}</h1>;
     }
 
+    if (!showAtm) {
+        // Refresh Map on toogle ATM -> MAP
+        getLocation();
+        // End of call for the map
+    }
+
     // display MAP
-    return <h1>{"Modal for the Map."}</h1>;
+    return <div id={"mapid"} />;
 }
 
 function AtmButton(props) {
