5961fc4d23adea8c161d7323862b379e205f29fe
Retro <mossiat.jeoffrey@outlook.com>
2019-09-13 10:30:34 +0200

    Fix : Set a display limit to terminals collection

--- src/server/index.js
+++ src/server/index.js
@@ -16,10 +16,34 @@ const app = express();
 app.use(express.static(path.resolve(__dirname, "../../bin/client")));
 
 app.get("/hello", (req, res) => {
+    // eslint-disable-next-line no-console
     console.log(`ℹ️  (${req.method.toUpperCase()}) ${req.url}`);
     res.send("Hello, World!");
 });
 
 app.listen(APP_PORT, () =>
+    // eslint-disable-next-line no-console
     console.log(`🚀 Server is listening on port ${APP_PORT}.`),
 );
+
+const MongoClient = require("mongodb").MongoClient;
+const assert = require("assert");
+const url = "mongodb://dev:dev@mongo:27017";
+
+MongoClient.connect(url, (err, client) => {
+    assert.equal(null, err);
+
+    const db = client.db("trouvkash");
+    const terminals = db.collection("terminals");
+    console.log(
+        terminals
+            .find()
+            .limit(20)
+            .toArray((err, items) => {
+                client.close();
+                console.log(items);
+                console.log(err);
+            }),
+    );
+    client.close();
+});
