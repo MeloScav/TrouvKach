950e2bce0f42ffebfc9dda5f6f660c17a7be2872
Méloé Scavezzoni <meloscavezzoni@gmail.com>
2019-09-16 11:43:55 +0200

    style button

--- src/client/components/react-popup.js
+++ src/client/components/react-popup.js
@@ -3,14 +3,29 @@ import {Marker, Popup} from "react-leaflet";
 
 const position = [50.6326, 5.5797];
 
+const styleTitle = {
+    fontSize: "16px",
+    fontWeight: "bold",
+};
+const styleBody = {
+    margin: 0,
+    padding: 0,
+    fontSize: "13px",
+};
+
 const ReactPopup = () => (
     <Marker position={position}>
         <Popup>
-            <div className={"titleBank"}>{"Name Bank"}</div>
-            <div className={"bodyBank"}>
-                {"Informations"}
+            <div style={styleTitle}>{"Name Bank"}</div>
+            <div style={styleBody}>
+                <ul className={"listPopup"}>
+                    <li>{"Informations"}</li>
+                    <li>{"lalala"}</li>
+                </ul>
                 <div>
-                    <button type={"button"}>{"see more"}</button>
+                    <button type={"button"} className={"buttonPopup"}>
+                        {"see more"}
+                    </button>
                 </div>
             </div>
         </Popup>
