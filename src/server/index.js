98b3ba7a9d86fa3ef07f3ac32dc46bd4ef243d0a
Retro <mossiat.jeoffrey@outlook.com>
2019-09-17 10:05:59 +0200

    Add: Road to bank name parameter in server.js at localhost/bank/Belfius

--- src/server/index.js
+++ src/server/index.js
@@ -3,35 +3,35 @@ import express from "express";
 import path from "path";
 
 const {APP_PORT} = process.env;
-
 const app = express();
+const MongoClient = require("mongodb").MongoClient;
 
 app.use(express.static(path.resolve(__dirname, "../../bin/client")));
 
 app.get("/bank/:name", (req, res) => {
-    res.send(req.params.name);
+    const uri = "mongodb+srv://admin:admin@cash-dejic.gcp.mongodb.net/test";
+    const client = new MongoClient(uri, {useNewUrlParser: true});
+
+    let report = "";
+
+    client.connect(() => {
+        const collection = client.db("test").collection("Banks");
+        console.log(
+            collection
+                .find()
+                .limit(20)
+                .toArray((err, items) => {
+                    client.close();
+                    report = report + String(items[0].name);
+                    res.send(report);
+                    console.log(err);
+                }),
+        );
+        client.close();
+    });
 });
 
 app.listen(APP_PORT, () =>
     // eslint-disable-next-line no-console
     console.log(`🚀 Server is listening on port ${APP_PORT}.`),
 );
-
-const MongoClient = require("mongodb").MongoClient;
-
-const uri = "mongodb+srv://admin:admin@cash-dejic.gcp.mongodb.net/test2";
-const client = new MongoClient(uri, {useNewUrlParser: true});
-client.connect(() => {
-    const collection = client.db("test2").collection("Terminals");
-    console.log(
-        collection
-            .find()
-            .limit(20)
-            .toArray((err, items) => {
-                client.close();
-                console.log(items);
-                console.log(err);
-            }),
-    );
-    client.close();
-});
