7db837daf13fe332a5cc8b05caf3a509a9a046b2
Méloé Scavezzoni <meloscavezzoni@gmail.com>
2019-09-18 14:54:39 +0200

    add buttons modal

--- src/client/components/modal.js
+++ src/client/components/modal.js
@@ -4,7 +4,10 @@ import Button from "./button";
 import buttonClose from "./assets/close-button.png";
 
 const styleModal = {
-    display: "block",
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center",
+    alignContent: "center",
     backgroundColor: "#fff",
     width: "50%",
     zIndex: "1000",
@@ -13,17 +16,33 @@ const styleModal = {
     left: "25%",
     border: "20px solid rgba(0, 0, 0, 0.3)",
     backgroundClip: "padding-box",
+    borderRadius: "1%",
     paddingLeft: "15px",
     paddingRight: "5px",
     paddingBottom: "10px",
 };
-const styleTitleModal = {};
+const styleDiv2 = {
+    alignSelf: "center",
+};
+const styleTitleModal = {
+    textAlign: "center",
+};
+const styleText = {
+    alignSelf: "center",
+};
 const styleButtonM = {
+    alignSelf: "start",
     borderColor: "transparent",
     backgroundColor: "transparent",
     width: "30px",
     marginTop: "7px",
 };
+const styleDivButtons = {
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "space-evenly",
+};
+const styleButotnDelete = {};
 
 // Modal with settings "onClose", which will be called at the click
 // Allows the return to the initial value of the popup
@@ -38,23 +57,28 @@ const Modal = ({onClose}) => {
     return createPortal(
         <div style={styleModal}>
             <div>
-                <div>
-                    {/* on Click, we put in false (the show) with setShow */}
-                    <Button
-                        class={"buttonModal"}
-                        style={styleButtonM}
-                        onClick={() => {
-                            setShow(false);
-                            onClose();
-                        }}
-                        img={buttonClose}
-                        alt={"button close"}
-                    />
-                </div>
+                {/* on Click, we put in false (the show) with setShow */}
+                <Button
+                    class={"buttonModal"}
+                    style={styleButtonM}
+                    onClick={() => {
+                        setShow(false);
+                        onClose();
+                    }}
+                    img={buttonClose}
+                    alt={"button close"}
+                />
+            </div>
+            <div style={styleDiv2}>
                 <h4 style={styleTitleModal}>{"Name Bank"}</h4>
-                <div>
+                <div style={styleText}>
                     <p>{"Information bank"}</p>
+                    <p>{"laaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"}</p>
                 </div>
+            </div>
+            <div style={styleDivButtons}>
+                <Button style={styleButotnDelete} value={"Delete"} />
+                <Button value={"Update"} />
             </div>
         </div>,
         document.querySelector("#modal-container"),
